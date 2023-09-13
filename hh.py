fa4e7991975bf36580352f72807f6b1b114f6d67
skyeverywhere <128362972+skyeverywhere@users.noreply.github.com>
2023-09-13 12:05:02 +0800

    修改文件

--- hh.py
+++ hh.py
@@ -1,8 +1,13 @@
 import torch
 import torch.nn
+import numpy as np
 
-print("hhhh")
-print("hhhh")
-print("hhhh")
-print("hhhh")
-print("hhhh")
+a = np.array([[1, 2, 3], [4, 5, 6]])
+a = torch.from_numpy(a)
+
+print(a)
+a = a.reshape(3, 2)
+print(a)
+
+a = torch.permute(a, dims=(1, 0))
+print(a)
